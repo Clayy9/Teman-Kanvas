5718c40b75a2c1415ffb6cca7e57f1c75b161918
Clayy9 <clay.aiken@itbss.ac.id>
2023-03-12 23:25:20 +0700

    REVISI - BENEFIT, FOOTER, RAW TESTIMONI, ADDING NAVBAR, ADDING PRODUCT

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ function scrollFunction() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
+
+
+
+    
